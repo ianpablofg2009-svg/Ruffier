ced9cce545734e201d7db03e374135eccf8132b6
IanF <ianpablofg2009@gmail.com>
2026-07-30 18:54:33 -0300

    estructura ventana inicio completa

--- first_win.py
+++ first_win.py
@@ -11,5 +11,12 @@ class Home(QWidget):
         self.setLayout(layout_home)
         #crear un label para crear el subtítulo de la aplicación
         self.lbl_subtitle = QLabel("App Ruffier")
+        #crear un label para información de la aplicación
+        self.lbl_info = QLabel("Esta aplicacion permite calcular el indice de Ruffier, que es un indice de rendimiento cardiovascular.")
+        #crear un botón para iniciar la prueba
+        self.btn_start = QPushButton("Iniciar prueba")
+
         #asignar widgets al layout
         layout_home.addWidget(self.lbl_subtitle)
+        layout_home.addWidget(self.lbl_info)
+        layout_home.addWidget(self.btn_start)
